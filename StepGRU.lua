4381f58e8a4a6fbcfe56c7032caa3c27e6a75898
ano <anoidgit@users.noreply.github.com>
2018-01-06 19:46:17 +0800

    fix typo

--- StepGRU.lua
+++ StepGRU.lua
@@ -2,6 +2,6 @@ local StepGRU, parent = torch.class('srnn.StepGRU', 'srnn.AbstractRecurrent')
 
 function StepGRU:buildStep(inputSize, outputSize, layers, dropout)
 
-	return srnn.units.getGRUUnit(inputSize, outputSize, layers, dropou)
+	return srnn.units.getGRUUnit(inputSize, outputSize, layers, dropout)
 
 end
